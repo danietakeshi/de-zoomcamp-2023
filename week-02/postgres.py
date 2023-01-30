35c88fcfb04c6eb3cc255c4605f11e7e5c20bdaf
Daniel Takeshi <martins.takeshi@gmail.com>
2023-01-30 15:53:25 -0300

    Introduction to Prefect concepts Completed

--- week-02/postgres.py
+++ week-02/postgres.py
@@ -5,10 +5,13 @@ import pandas as pd
 from time import time
 from sqlalchemy import create_engine
 from prefect import flow, task
+from prefect.tasks import task_input_hash
 import os
+from datetime import timedelta
+from prefect_sqlalchemy import SqlAlchemyConnector
 
-@task(log_prints=True, retries=3)
-def ingest_data(user, password, host, port, db, table_name, csv_url):
+@task(log_prints=True, retries=0, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
+def extract_data(csv_url):
     csv_name = 'output.csv'
 
     if csv_url.endswith('.csv.gz'):
@@ -17,14 +20,6 @@ def ingest_data(user, password, host, port, db, table_name, csv_url):
     else:
         os.system(f'wget {csv_url} -O {csv_name}')
 
-    df = pd.read_csv(csv_name, nrows=100)
-
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
-
-    engine.connect()
-
-    pd.io.sql.get_schema(df, table_name, con=engine)
-
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
     df = next(df_iter)
@@ -32,35 +27,36 @@ def ingest_data(user, password, host, port, db, table_name, csv_url):
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
-    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+    return df
 
-    df.to_sql(name=table_name, con=engine, if_exists='append')
+@task(log_prints=True)
+def transform_data(df):
+    print(f"pre: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
+    df = df[df['passenger_count'] != 0]
+    print(f"post: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
+    return df
 
-    while True:
-        t_start = time()
 
-        df = next(df_iter)
+@task(log_prints=True, retries=0)
+def ingest_data(table_name, df):
+    connection_block = SqlAlchemyConnector.load("postgres-connector")
 
-        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
+    with connection_block.get_connection(begin=False) as engine:
+        df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
         df.to_sql(name=table_name, con=engine, if_exists='append')
 
-        t_end = time()
-        print('Inserted another chunk..., took %.3f second' % (t_end - t_start))
+@flow(name="Subflow", log_prints=True)
+def log_subflow(table_name: str):
+    print(f'Logging Subflow for: {table_name}')
 
 @flow(name="Ingest Flow")
-def main_flow():
-    user = "user@domain.com"
-    password = "root"
-    host = "192.168.15.46"
-    port = "5432"
-    db = "ny_taxi"
-    table_name = "yellow_taxi_trips"
+def main_flow(table_name: str):
     csv_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
-
-    ingest_data(user, password, host, port, db, table_name, csv_url)
+    log_subflow(table_name)
+    raw_data = extract_data(csv_url)
+    data = transform_data(raw_data)
+    ingest_data(table_name, data)
 
 
 if __name__ == '__main__':
-    main_flow()
+    main_flow("yellow_taxi_trips")
